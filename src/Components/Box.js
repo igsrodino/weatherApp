7f1e3071f58095599967568040966f1bced50c5a
igsrodino <igs_rodino@hotmail.com>
2019-11-17 11:36:01 +1000

    added Box.js and .css, added Header.js and .css

---
+++ src/Components/Box.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Box.css';
+import Header from './Header.js'
+
+function Box() {
+  return (
+      <div className="Box">
+              <Header/>
+
+      </div>
+
+
+  );
+}
+
+export default Box;
